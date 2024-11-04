a1fe9cfd9920152e4f75bd0d06b47d4f064873fe
owen <owentobias21@gmail.com>
2024-11-04 19:47:53 +0700

    refactor: move constants

---
+++ backend/constants.py
@@ -0,0 +1,10 @@
+# Cube Constants
+MAGIC_CUBE_SIZE = 5 # Size of the magic cube which is 5x5x5
+MAGIC_CONSTANT = 315  # Target sum for rows, columns, and diagonals
+MAGIC_NUMBERS = list(range(1, MAGIC_CUBE_SIZE ** 3 + 1))
+
+
+# Genetic Algorithm Parameters
+POPULATION_SIZES = [100]  # Example population sizes
+ITERATION_VARIANTS = [500]  # Example iteration counts
+EXPERIMENT_RUNS = 3
